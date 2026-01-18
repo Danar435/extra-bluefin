e83b415fc69dc79ce28bba40220d98480d5567c6
Kristiyan <2003kristiyan@gmail.com>
2026-01-19 00:51:17 +0100

    Update aliases and install sunshine

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,17 +7,18 @@ set -ouex pipefail
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y copr enable faugus/faugus-launcher
+dnf5 -y copr enable lizardbyte/beta
 
 ### Install packages
 
-dnf5 -y install steam gamescope gamemode mangohud
-dnf5 -y install faugus-launcher waydroid
+dnf5 -y install steam gamescope gamemode mangohud faugus-launcher waydroid sunshine
 
 ### Cleaning up
 
 dnf5 -y remove rpmfusion-free-release
 dnf5 -y remove rpmfusion-nonfree-release
 dnf5 -y copr disable faugus/faugus-launcher
+dnf5 -y copr disable lizardbyte/beta
 
 ### Install vscode
 
@@ -32,14 +33,13 @@ dnf5 -y copr disable faugus/faugus-launcher
 #sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
 #dnf5 -y install --enablerepo=code code
 
+### Setup sunshine
+
+setcap 'cap_sys_admin+p' $(readlink -f /usr/bin/sunshine)
+
 ### Setup aliases
 
-play() {
-    MANGOHUD=1 gamemoderun gamescope -W 1920 -H 1080 -f -- "$@"
-}
-play-fsr() {
-    MANGOHUD=1 gamemoderun gamescope -W 1920 -H 1080 -f -F fsr -- "$@"
-}
-play-int() {
-    MANGOHUD=1 gamemoderun gamescope -W 1920 -H 1080 -f -F nearest -S integer -- "$@"
-}
+echo 'play()     { gamemoderun gamescope -W 1920 -H 1080 -f -- mangohud "$@" }
+play-fsr() { gamemoderun gamescope -W 1920 -H 1080 -f -F fsr -- mangohud "$@"; }
+play-int() { gamemoderun gamescope -W 1920 -H 1080 -f -F nearest -S integer -- mangohud "$@"; }
+' >> /etc/profile.d/play.sh
