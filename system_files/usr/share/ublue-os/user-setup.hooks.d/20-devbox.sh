cb42347e90fd61eb737fffa1caff90d6953f42cf
Kristiyan <2003kristiyan@gmail.com>
2026-02-17 13:17:00 +0100

    Devbox minor fix

--- system_files/usr/share/ublue-os/user-setup.hooks.d/20-devbox.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/20-devbox.sh
@@ -4,6 +4,6 @@ source /usr/lib/ublue/setup-services/libsetup.sh
 
 version-script devbox user 1 || exit 1
 
-if ! distrobox list | grep -q "my-custom-distrobox"; then
+if ! distrobox list | grep -q "devbox"; then
     distrobox create --name devbox --image quay.io/toolbx/arch-toolbox:latest --home "$HOME/.devbox"  --init
 fi
