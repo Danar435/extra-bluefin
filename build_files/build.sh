50487bf3162d4e76b9dc07a8aa03ffcdd283e166
Kristiyan <2003kristiyan@gmail.com>
2026-03-09 16:22:12 +0100

    Remove extensions temporarily

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ cp -r /ctx/system_files/* /
 
 # Run build scripts
 
-/ctx/build_files/build-extensions.sh
+#/ctx/build_files/build-extensions.sh
 /ctx/build_files/build-packages.sh
 
 # Restore default glob behavior
