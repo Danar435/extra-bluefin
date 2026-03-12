0b9b8c10c6a1601b53d36b66c16737c597c1bee9
Kristiyan <2003kristiyan@gmail.com>
2026-03-12 13:39:05 +0100

    Fix gamescope performance

--- build_files/build-packages.sh
+++ build_files/build-packages.sh
@@ -11,6 +11,7 @@ source /ctx/build_files/copr-helpers.sh
 copr_install_isolated "faugus/faugus-launcher" faugus-launcher
 copr_install_isolated "lizardbyte/beta" sunshine
 
-# Setup sunshine
+# Setup packages
 
 setcap 'cap_sys_admin+p' $(readlink -f /usr/bin/sunshine)
+setcap 'CAP_SYS_NICE=eip'  $(readlink -f /usr/bin/gamescope)
